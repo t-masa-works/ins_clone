7ff4ecb7525bac025131c7ec7443158ddf11ae83
tobe masa <t.masa.works@gmail.com>
2023-08-06 20:28:32 +0900

    メールバリデーション、アドレスユニーク制約

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_06_111526) do
+ActiveRecord::Schema.define(version: 2023_08_06_112646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2023_08_06_111526) do
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
