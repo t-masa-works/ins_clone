dfbe0ae780dfebff539b276a6e5a805ee79f01c8
tobe masa <t.masa.works@gmail.com>
2023-09-14 10:13:46 +0900

    初期ルート変更

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,6 @@
 class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
   belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root'users#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :pictures
